d27b000e5befbf8d7278ad212447ba2e73a88e06
Jonathan Lawrence <Jonathan.Lawrence@utdallas.edu>
2019-10-18 12:04:14 -0500

    Added records to DB

--- test_insert.py
+++ test_insert.py
@@ -3,5 +3,5 @@ from boto3.dynamodb.conditions import Key
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Senior_Design_Table')
 table.put_item(
-    Item={'title':'Hello'}
+    Item={'Title':'Hello'}
 )
